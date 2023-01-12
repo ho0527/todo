4c68b816ec2ecdb70e7cb94f1e95d0bd7eb3b68d
ho0527 <chris960527ho@gmail.com>
2023-01-12 21:54:46 +0800

    202301121 todobox

--- todobox.js
+++ todobox.js
@@ -22,3 +22,57 @@ for(let i=0;i<todoeditblock.length;i=i+1){
         console.log(todoeditblock[i]);
     })
 }
+
+let boxid=undefined
+document.querySelectorAll('.work-box').forEach(function(element){
+    element.addEventListener("mousedown",function(){
+        boxid=this.id
+    })
+})
+document.querySelectorAll(".todo").forEach(function(element){
+    element.addEventListener("mousemove",function(event){
+        if(boxid==undefined){
+            return
+        }
+        let box=document.getElementById(boxid)
+        let y=event.pageY
+        let start=Math.floor((y-100)/50)
+        let height=parseFloat(box.style.height.replace("px",""))/50
+        if(start<0){
+            start=0
+        }
+        if(start+height>48){
+            start=48-height
+        }
+        let end=start+height
+        let top=start*50+110
+        box.style.top=top
+        if(start%2==0){
+            start=String(start/2).padStart(2,"0")+":00"
+        }else{
+            start=String(start/2- 0.5).padStart(2,"0")+":30"
+        }
+        if(end%2==0){
+            end=String(end/2).padStart(2,"0")+":00"
+        }else{
+            end=String(end/2- 0.5).padStart(2,"0")+":30"
+        }
+    })
+})
+document.querySelectorAll(".todo").forEach(function(element){
+    element.addEventListener("mouseup",function(){
+        console.log("up")
+        let box=document.getElementById(boxid)
+        let height=parseFloat(box.style.height.replace("px",""))/50
+        let start=parseFloat(box.style.top.replace("px","")-110)/50
+        let end=start+height
+        let xhr=new XMLHttpRequest()
+        xhr.open("GET","userWelcome.php?boxid="+boxid+"&start="+start+"&end="+end,true)
+        xhr.onreadystatechange=function(){
+            if(xhr.readyState==4&&xhr.status==200){
+                //location.reload()
+            }
+        }
+        xhr.send()
+    })
+})
--- userdef.php
+++ userdef.php
@@ -20,19 +20,19 @@
                 $min=substr($row[4],3,5)-substr($row[3],3,5);
                 if($start_time>=960){
                     ?>
-                    <div class="work-box" draggable="true" value="<?= $row[0]; ?>" style="height:<?= ($hr*30)+(($min/30)*15)+15; ?>px;top:<?= 145+($start_time/2)+20; ?>px;left:230px;">
+                    <div class="work-box" id="<?= $row[0]; ?>" draggable="true" value="<?= $row[0]; ?>" style="height:<?= ($hr*30)+(($min/30)*15)+15; ?>px;top:<?= 145+($start_time/2)+20; ?>px;left:230px;">
                         <?php data($row); ?>
                     </div>
                     <?php
                 }elseif($start_time>=600){
                     ?>
-                    <div class="work-box" draggable="true" value="<?= $row[0]; ?>" style="height:<?= ($hr*30)+(($min/30)*15)+15; ?>px;top:<?= 145+($start_time/2)+15; ?>x;left:230px;">
+                    <div class="work-box" id="<?= $row[0]; ?>" draggable="true" value="<?= $row[0]; ?>" style="height:<?= ($hr*30)+(($min/30)*15)+15; ?>px;top:<?= 145+($start_time/2)+15; ?>x;left:230px;">
                         <?php data($row); ?>
                     </div>
                     <?php
                 }else{
                     ?>
-                    <div class="work-box" draggable="true" value="<?= $row[0]; ?>" style="height:<?= ($hr*30)+(($min/30)*15)+15; ?>px;top:<?= 145+($start_time/2)-15; ?>px;left:230px;">
+                    <div class="work-box" id="<?= $row[0]; ?>" draggable="true" value="<?= $row[0]; ?>" style="height:<?= ($hr*30)+(($min/30)*15)+15; ?>px;top:<?= 145+($start_time/2)-15; ?>px;left:230px;">
                         <?php data($row); ?>
                     </div>
                     <?php
@@ -49,19 +49,19 @@
                 $min=substr($row[4],3,5)-substr($row[3],3,5);
                 if($end_time>=960){
                     ?>
-                    <div class="work-box" draggable="true" value="<?= $row[0]; ?>" style="height:<?= ($hr*30)+(($min/30)*15)+15; ?>px;top:<?= 145+(($end_time-1155)/2)+335; ?>px;left:230px;">
+                    <div class="work-box" id="<?= $row[0]; ?>" draggable="true" value="<?= $row[0]; ?>" style="height:<?= ($hr*30)+(($min/30)*15)+15; ?>px;top:<?= 145+(($end_time-1155)/2)+335; ?>px;left:230px;">
                         <?php data($row); ?>
                     </div>
                     <?php
                 }elseif($end_time>=600){
                     ?>
-                    <div class="work-box" draggable="true" value="<?= $row[0]; ?>" style="height:<?= ($hr*30)+(($min/30)*15)+15; ?>px;top:<?= 145+(($end_time-125)/2)+165; ?>px;left:230px;">
+                    <div class="work-box" id="<?= $row[0]; ?>" draggable="true" value="<?= $row[0]; ?>" style="height:<?= ($hr*30)+(($min/30)*15)+15; ?>px;top:<?= 145+(($end_time-125)/2)+165; ?>px;left:230px;">
                         <?php data($row); ?>
                     </div>
                     <?php
                 }else{
                     ?>
-                    <div class="work-box" draggable="true" value="<?= $row[0]; ?>" style="height:<?= ($hr*30)+(($min/30)*15)+15; ?>px;top:<?= 145+(($end_time+960)/2)-110; ?>px;left:230px;">
+                    <div class="work-box" id="<?= $row[0]; ?>" draggable="true" value="<?= $row[0]; ?>" style="height:<?= ($hr*30)+(($min/30)*15)+15; ?>px;top:<?= 145+(($end_time+960)/2)-110; ?>px;left:230px;">
                         <?php data($row); ?>
                     </div>
                     <?php
